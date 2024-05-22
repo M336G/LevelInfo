10e6b5e4f5c7b912da24045bf5af64cad3492b0d
M336 <75208577+M336G@users.noreply.github.com>
2024-05-22 16:47:46 +0200

    v1.3.4

--- src/main.cpp
+++ src/main.cpp
@@ -6,34 +6,30 @@ class $modify(LevelInfoLayer) {
 			return false;
 
 		// Get all the settings values
-		auto requestedStarsToggle = geode::Mod::get()->getSettingValue<bool>("show-requested-stars");
-		auto featuredRankToggle = geode::Mod::get()->getSettingValue<bool>("show-featured-rank");
-		auto objectCountToggle = geode::Mod::get()->getSettingValue<bool>("show-object-count");
-		auto gameVersionToggle = geode::Mod::get()->getSettingValue<bool>("show-game-version");
-		auto levelVersionToggle = geode::Mod::get()->getSettingValue<bool>("show-level-version");
-		auto twoPlayerModeToggle = geode::Mod::get()->getSettingValue<bool>("show-two-player-mode");
-		auto editorTimeToggle = geode::Mod::get()->getSettingValue<bool>("show-editor-time");
-		auto editorTimeCopiesToggle = geode::Mod::get()->getSettingValue<bool>("show-editor-time-copies");
-		auto totalAttemptsToggle = geode::Mod::get()->getSettingValue<bool>("show-total-attempts");
-		auto totalJumpsToggle = geode::Mod::get()->getSettingValue<bool>("show-total-jumps");
-		auto clicksToggle = geode::Mod::get()->getSettingValue<bool>("show-clicks");
-		auto attemptTimeToggle = geode::Mod::get()->getSettingValue<bool>("show-attempt-time");
+		const auto requestedStarsToggle = geode::Mod::get()->getSettingValue<bool>("show-requested-stars");
+		const auto featuredRankToggle = geode::Mod::get()->getSettingValue<bool>("show-featured-rank");
+		const auto objectCountToggle = geode::Mod::get()->getSettingValue<bool>("show-object-count");
+		const auto gameVersionToggle = geode::Mod::get()->getSettingValue<bool>("show-game-version");
+		const auto levelVersionToggle = geode::Mod::get()->getSettingValue<bool>("show-level-version");
+		const auto originalLevelToggle = geode::Mod::get()->getSettingValue<bool>("show-original-level-id");
+		const auto twoPlayerModeToggle = geode::Mod::get()->getSettingValue<bool>("show-two-player-mode");
+		const auto editorTimeToggle = geode::Mod::get()->getSettingValue<bool>("show-editor-time");
+		const auto editorTimeCopiesToggle = geode::Mod::get()->getSettingValue<bool>("show-editor-time-copies");
+		const auto totalAttemptsToggle = geode::Mod::get()->getSettingValue<bool>("show-total-attempts");
+		const auto totalJumpsToggle = geode::Mod::get()->getSettingValue<bool>("show-total-jumps");
+		const auto clicksToggle = geode::Mod::get()->getSettingValue<bool>("show-clicks");
+		const auto attemptTimeToggle = geode::Mod::get()->getSettingValue<bool>("show-attempt-time");
 
 		// If all of those values are false, don't display the label text
 		if (!requestedStarsToggle && !featuredRankToggle && !objectCountToggle && !gameVersionToggle && !levelVersionToggle && !twoPlayerModeToggle && !editorTimeToggle && !editorTimeCopiesToggle && !totalAttemptsToggle && !totalJumpsToggle && !clicksToggle && !attemptTimeToggle) return true;
 
-		// Get the text size defined by the user
-		auto textSize = geode::Mod::get()->getSettingValue<double>("text-size");
-		// If the text size is 0, don't display the label
+		const auto textSize = geode::Mod::get()->getSettingValue<double>("text-size");
 		if (textSize <= 0) return true;
 
-		// Get the text opacity defined by the user
-		auto textOpacity = geode::Mod::get()->getSettingValue<int64_t>("text-opacity");
-		// If the text opacity is 0, don't display the label
+		const auto textOpacity = geode::Mod::get()->getSettingValue<int64_t>("text-opacity");
 		if (textOpacity <= 0) return true;
 
-		// Get the text color defined by the user
-		auto textColor = geode::Mod::get()->getSettingValue<cocos2d::ccColor3B>("text-color");
+		const auto textColor = geode::Mod::get()->getSettingValue<cocos2d::ccColor3B>("text-color");
 
 		// Get the size of the window
 		const auto winSize = cocos2d::CCDirector::get()->getWinSize();
@@ -51,6 +47,7 @@ class $modify(LevelInfoLayer) {
 				case 0:
 				case 65535:
 					levelString = geode::prelude::ZipUtils::decompressString(level->m_levelString, false, 0);
+
 					#ifdef __APPLE__
 					labelString << fmt::format("Object Count: ~{}\n", std::count(levelString.begin(), levelString.end(), ';'));
 					#else
@@ -89,18 +86,20 @@ class $modify(LevelInfoLayer) {
 			}
 		}
 
-		if (twoPlayerModeToggle) labelString << fmt::format("2 Player Mode: {}\n", level->m_twoPlayerMode);
+		if (twoPlayerModeToggle) labelString << fmt::format("2-Player Mode: {}\n", level->m_twoPlayerMode);
 
 		if (levelVersionToggle) labelString << fmt::format("Level Version: {}\n", level->m_levelVersion);
+		// Yes.
+		if (originalLevelToggle) labelString << fmt::format("Original ID: {}\n", (static_cast<int>(level->m_originalLevel) == static_cast<int>(level->m_levelID) || static_cast<int>(level->m_originalLevel) == 0) ? "N/A" : std::to_string(static_cast<int>(level->m_originalLevel)));
 
 		if (editorTimeToggle) {
 			std::chrono::seconds seconds(level->m_workingTime);
-			labelString << fmt::format("Edit. Time: {}h{}m{}s \n", duration_cast<std::chrono::hours>(seconds).count(), duration_cast<std::chrono::minutes>(seconds).count() % 60, seconds.count() % 60);
+			labelString << fmt::format("Editor: {}h{}m{}s \n", duration_cast<std::chrono::hours>(seconds).count(), duration_cast<std::chrono::minutes>(seconds).count() % 60, seconds.count() % 60);
 		}
 
 		if (editorTimeCopiesToggle) {
 			std::chrono::seconds seconds(level->m_workingTime + level->m_workingTime2);
-			labelString << fmt::format("Edit. Time (+cop.): {}h{}m{}s \n", duration_cast<std::chrono::hours>(seconds).count(), duration_cast<std::chrono::minutes>(seconds).count() % 60, seconds.count() % 60);
+			labelString << fmt::format("Edit. (+cop.): {}h{}m{}s \n", duration_cast<std::chrono::hours>(seconds).count(), duration_cast<std::chrono::minutes>(seconds).count() % 60, seconds.count() % 60);
 		}
 
 		if (totalAttemptsToggle) labelString << fmt::format("Total Attempts: {}\n", static_cast<int>(level->m_attempts));
@@ -115,10 +114,10 @@ class $modify(LevelInfoLayer) {
 		}
 
 		// Display the label
-		auto label = cocos2d::CCLabelBMFont::create(labelString.str().c_str(), "bigFont.fnt");
+		const auto label = cocos2d::CCLabelBMFont::create(labelString.str().c_str(), "bigFont.fnt");
 		label->setID("level-info-label"_spr);
 		label->setPosition(winSize / 1.4);
-		label->setPositionX(100);
+		label->setPositionX(110);
 		label->setScale(textSize);
 		label->setOpacity(static_cast<int64_t>(round(static_cast<double>(textOpacity) / 100 * 255)));
 		label->setColor(textColor);
